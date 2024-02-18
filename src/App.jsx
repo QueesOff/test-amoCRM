2efd74939b72ba0e4dc3815687fec647a8d2aa4f
DaimenGGer <daimenkg@gmail.com>
2024-02-18 18:10:26 +0600

    v2

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>amoCRM тестовое задание Сабыржанов Барсбек</h1>
+    </div>
+  );
+}
+
+export default App;
